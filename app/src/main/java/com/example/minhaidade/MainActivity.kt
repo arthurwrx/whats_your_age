800c0307f9631de4fd64270f4310c1290dd595aa
arthurwrx <arthurwrx@outlook.com>
2024-03-04 22:16:50 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/minhaidade/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.minhaidade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.minhaidade.ui.theme.MinhaIdadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MinhaIdadeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                MeuComponente()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MeuComponente() {
+    Column() {
+        Text(
+            text = "Qual a sua idade?",
+            color = Color.Red,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+
+        )
+        Text(
+            text = "Pressione os botões para informar a sua idade",
+            fontSize = 12.sp,
+            color = Color.Blue,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = "25",
+            fontSize = 48.sp,
+            color = Color.Red,
+
+
+        )
+        Row() {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "+")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "-")
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun MeuComponentePreview() {
+    MeuComponente()
+}
