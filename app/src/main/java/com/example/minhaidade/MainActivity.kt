5cab4a6010260c77c51dadb67e19eeec71845123
arthurwrx <arthurwrx@outlook.com>
2024-03-04 23:02:29 -0300

    Ajustei os botões e visualização

--- app/src/main/java/com/example/minhaidade/MainActivity.kt
+++ app/src/main/java/com/example/minhaidade/MainActivity.kt
@@ -3,20 +3,27 @@ package com.example.minhaidade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.minhaidade.ui.theme.MinhaIdadeTheme
 
@@ -39,7 +46,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MeuComponente() {
-    Column() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom) {
         Text(
             text = "Qual a sua idade?",
             color = Color.Red,
@@ -63,17 +71,27 @@ fun MeuComponente() {
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
         )
-        Row() {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "+")
-            }
-            Button(onClick = { /*TODO*/ }) {
+
+        Row(horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF88c6a5)),
+                modifier = Modifier.size(84.dp),
+                shape = RectangleShape
+            ) {
                 Text(text = "-")
+            }
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF88c6a5)),
+                modifier = Modifier.size(84.dp),
+                shape = RectangleShape
+            ) {
+                Text(text = "+")
             }
         }
     }
 }
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun MeuComponentePreview() {
     MeuComponente()
